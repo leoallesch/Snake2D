689b831e923010e59710d5647a85bb7b3de9d7f6
Leo Allesch <leoallesch@gmail.com>
2021-09-17 09:35:27 -0400

    ok

--- Game.cpp
+++ Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <iostream>
 
 Game::Game()
 	: mWindow(sf::VideoMode(WIDTH, LENGTH), "Snake")
@@ -10,22 +11,95 @@ Game::Game()
 }
 
 void Game::run() {
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
 	while (mWindow.isOpen()) {
 		processEvents();
-		update();
+		handleInput();
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > TimePerFrame) {
+			timeSinceLastUpdate -= TimePerFrame;
+			processEvents();
+			handleInput();
+			update(TimePerFrame);
+		}
 		render();
 	}
 }
 void Game::processEvents() {
 	sf::Event event;
 	while (mWindow.pollEvent(event)) {
-		if (event.type == sf::Event::Closed) {
+		switch (event.type) {
+		case sf::Event::Closed:
 			mWindow.close();
+			break;
+		case sf::Event::KeyPressed:
+			switch (event.key.code) {
+				case sf::Keyboard::W:
+					if (dir != DOWN)
+						dir = UP;
+					break;
+				case sf::Keyboard::S:
+					if (dir != UP)
+						dir = DOWN;
+					break;
+				case sf::Keyboard::A:
+					if (dir != RIGHT)
+						dir = LEFT;
+					break;
+				case sf::Keyboard::D:
+					if (dir != LEFT)
+						dir = RIGHT;
+					break;
+				default:
+					break;
+			}
 		}
 	}
 }
 
-void Game::update() {
+void Game::handleInput() {
+	switch (dir) {
+		case UP:
+			moveUp = true;
+			moveDown = false;
+			moveLeft = false;
+			moveRight = false;
+			break;
+		case DOWN:
+			moveUp = false;
+			moveDown = true;
+			moveLeft = false;
+			moveRight = false;
+			break;
+		case LEFT:
+			moveUp = false;
+			moveDown = false;
+			moveLeft = true;
+			moveRight = false;
+			break;
+		case RIGHT:
+			moveUp = false;
+			moveDown = false;
+			moveLeft = false;
+			moveRight = true;
+			break;
+	}
+	std::cout << moveUp << " " << moveDown << " " << moveLeft << " " << moveRight << std::endl;
+}
+
+void Game::update(sf::Time TimePerFrame) {
+	movement = sf::Vector2f(0.f, 0.f);
+	if (moveUp)
+		movement.y -= speed;
+	else if (moveDown)
+		movement.y += speed;
+	else if (moveLeft)
+		movement.x -= speed;
+	else if (moveRight)
+		movement.x += speed;
+
+	mHead.move(movement * TimePerFrame.asSeconds());
 
 }
 
--- Game.h
+++ Game.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+
 const int WIDTH = 650;
 const int LENGTH = 500;
+const sf::Time TimePerFrame = sf::seconds(0.1f / 60.f);
 
 class Game {
 public:
@@ -10,9 +12,22 @@ public:
 	void run();
 private:
 	void processEvents();
-	void update();
+	void update(sf::Time TimePerFrame);
 	void render();
+
+	void handleInput();
 private:
 	sf::RenderWindow mWindow;
 	sf::RectangleShape mHead;
+
+	enum Direction {STOP=0,UP,DOWN,LEFT,RIGHT};
+	Direction dir = STOP;
+
+	bool moveUp = false;
+	bool moveDown = false;
+	bool moveLeft = false;
+	bool moveRight = false;
+
+	sf::Vector2f movement;
+	int speed = 25;
 };
