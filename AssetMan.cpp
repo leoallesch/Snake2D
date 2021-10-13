1c6a5acc0c4a8b46f31f609cd08c6ada215124a0
Leo Allesch <leoallesch@gmail.com>
2021-10-13 13:29:22 -0400

    3:24

---
+++ AssetMan.cpp
@@ -0,0 +1,39 @@
+#include "AssetMan.h"
+
+Engine::AssetMan::AssetMan()
+{
+
+}
+Engine::AssetMan::~AssetMan()
+{
+
+}
+
+void Engine::AssetMan::AddTexture(int id, const std::string& filePath, bool wantRepeated = false)
+{
+	auto texture = std::make_unique<sf::Texture>();
+
+	if (texture->loadFromFile(filePath))
+	{
+		texture->setRepeated(wantRepeated);
+		m_textures[id] = std::move(texture);
+	}
+}
+void Engine::AssetMan::AddFont(int id, const std::string& filePath)
+{
+	auto font = std::make_unique<sf::Font>();
+
+	if (font->loadFromFile(filePath))
+	{
+		m_fonts[id] = std::move(font);
+	}
+}
+
+const sf::Texture& Engine::AssetMan::GetTexture() const
+{
+
+}
+const sf::Font& Engine::AssetMan::GetFont() const
+{
+
+}
